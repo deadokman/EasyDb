55593c64fb14a8817f596ad68068e1e885df90c2
Nikita <Stepanov.NO@gazprom-neft.ru>
2019-03-28 14:51:44 +0500

    + startup page

--- EasyDb/View/StartupPage/StartUpPageControll.xaml.cs
+++ EasyDb/View/StartupPage/StartUpPageControll.xaml.cs
@@ -20,12 +20,35 @@ namespace EasyDb.View.StartupPage
     /// </summary>
     public partial class StartUpPageControll : UserControl
     {
+        private GridLength _leftColLength;
+
+        private GridLength _rightColLength;
+
+        private GridLength _expanderColLength;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="StartUpPageControll"/> class.
         /// </summary>
         public StartUpPageControll()
         {
             InitializeComponent();
+            _leftColLength = this.LeftColumn.Width;
+            _rightColLength = this.RightColumn.Width;
+            _expanderColLength = this.ExpanderColumn.Width;
+        }
+
+        private void PatchnotesExpander_OnExpanded(object sender, RoutedEventArgs e)
+        {
+            LeftColumn.Width = new GridLength(0, GridUnitType.Auto);
+            RightColumn.Width = new GridLength(0, GridUnitType.Auto);
+            ExpanderColumn.Width = new GridLength(1, GridUnitType.Star);
+        }
+
+        private void PatchnotesExpander_OnCollapsed(object sender, RoutedEventArgs e)
+        {
+            LeftColumn.Width = _leftColLength;
+            RightColumn.Width = _rightColLength;
+            ExpanderColumn.Width = _expanderColLength;
         }
     }
 }
